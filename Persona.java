2341b225342284bd0d9f5bcb21311be5826fe03a
DAGutierrezSierra <132411372+DAGutierrezSierra@users.noreply.github.com>
2024-04-23 00:44:12 -0600

    Agregando mas atributos
    Se agregan mas atributos para completar el objeto persona

--- Persona.java
+++ Persona.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package expedientedelictivo;
     import java.util.ArrayList;
     import java.util.HashMap;
@@ -15,10 +11,14 @@ package expedientedelictivo;
 public class Persona {
     private String nombre;
     private String direccion;
+    private String edad;
+    private String sexo;
 
-    public Persona(String nombre, String direccion) {
+    public Persona(String nombre, String direccion, String edad, String sexo) {
         this.nombre = nombre;
         this.direccion = direccion;
+        this.edad = edad;
+        this.sexo = sexo;          
     }
 
     public String getNombre() {
@@ -27,5 +27,13 @@ public class Persona {
 
     public String getDireccion() {
         return direccion;
+    }
+
+     public String getEdad() {
+        return edad;
+    }
+    
+    public String getSexo() {
+        return sexo;
     }
 }
