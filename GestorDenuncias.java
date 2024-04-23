2fff0edce8cf834e8c0c0c968584370044b95848
DAGutierrezSierra <132411372+DAGutierrezSierra@users.noreply.github.com>
2024-04-23 00:44:57 -0600

    Update GestorDenuncias.java

--- GestorDenuncias.java
+++ GestorDenuncias.java
@@ -4,6 +4,10 @@ package expedientedelictivo;
     import java.util.HashMap;
     import java.util.List;
     import java.util.Map;
+    import java.sql.Connection;
+    import java.sql.PreparedStatement;
+    import java.sql.ResultSet;
+    import java.sql.SQLException;
 /**
  *
  * @author betin
